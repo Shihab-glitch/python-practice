aba7cb1467d1b0f2ee4f362e448e6f4376fec50d
Shihab Ahmad <shihabahmadrafi13@gmail.com>
2025-01-20 06:28:57 +0600

    Create # Problem - 16 - Even & Odd Number Finder from 0 to N.py

---
+++ # Problem - 16 - Even & Odd Number Finder from 0 to N.py
@@ -0,0 +1,14 @@
+## Odd-Even Count
+
+n = int(input("Enter N : "))
+count_odd = 0
+count_even = 0
+
+for x in range(1, n + 1):
+    if not x % 2:
+        count_even += 1
+    else:
+        count_odd += 1
+
+print("Number of even numbers:", count_even)
+print("Number of odd numbers:", count_odd)
