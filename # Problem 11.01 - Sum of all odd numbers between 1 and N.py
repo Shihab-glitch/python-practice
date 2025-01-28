7782f34bb5a3cedfe93c296339ed577f7b8edcca
Shihab Ahmad <shihabahmadrafi13@gmail.com>
2025-01-28 16:51:45 +0600

    Update # Problem 11.01 - Sum of all odd numbers between 1 and N.py

--- # Problem 11.01 - Sum of all odd numbers between 1 and N.py
+++ # Problem 11.01 - Sum of all odd numbers between 1 and N.py
@@ -7,3 +7,10 @@ while d <= n:
    sum_odd += d
    d += 2
 print("Sum of odd numbers from 1 to", n, "is", sum_odd)
+
+
+# Basic Code to find sum of all odd numbers between 1 and 100
+sum_odd = 0
+for i in range(1, 101, 2):
+    sum_odd = sum_odd + i
+print("Sum of odd numbers from 1 to 100 is", sum_odd)
