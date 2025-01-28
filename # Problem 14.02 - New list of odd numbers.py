5cd752887140dc4e5a8d34e8edfc5e98eb246cb5
Shihab Ahmad <shihabahmadrafi13@gmail.com>
2025-01-28 17:28:21 +0600

    Create # Problem 14.02 - New list of odd numbers.py

---
+++ # Problem 14.02 - New list of odd numbers.py
@@ -0,0 +1,10 @@
+# Display odd numbers from a list
+
+a = [int(x) for x in input("Enter the elements : ").split()]
+print("Input list:", a)
+
+new_list = []
+for num in a:
+    if num % 2 != 0:
+        new_list.append(num)
+print("Odd numbers: ", new_list)
