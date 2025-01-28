8f340d38d9832ad8ec2f9d0ba0256e0c971da927
Shihab Ahmad <shihabahmadrafi13@gmail.com>
2025-01-28 17:21:42 +0600

    Update # Problem 04.01 - Finding largest and smallest number of three numbers.py

--- # Problem 04.01 - Finding largest and smallest number of three numbers.py
+++ # Problem 04.01 - Finding largest and smallest number of three numbers.py
@@ -13,7 +13,7 @@ if num01>num02 and num01>num03:
     elif num02==num03:
         print("The Second & Third Number both are of same value")
 
-if num02>num03 and num02>num01:
+elif num02>num03 and num02>num01:
     print("The Second Number is the largest")
     if num01<num03:
         print("The First Number is the smallest")
@@ -22,7 +22,7 @@ if num02>num03 and num02>num01:
     elif num03==num01:
         print("The First & Third Number both are of same value")
 
-if num03>num02 and num03>num01:
+elif num03>num02 and num03>num01:
     print("The Third Number is the largest")
     if num02<num01:
         print("The Second Number is the smallest")
@@ -30,3 +30,12 @@ if num03>num02 and num03>num01:
         print("The First Number is the smallest")
     elif num02==num01:
         print("The First & Second Number both are of same value")
+
+elif num01==num02 and num01>num03:
+    print("The First and Second Number are of same value and larger than The Third Number")
+elif num03==num01 and num01>num02:
+    print("The First and Third Number are of same value and larger than The Second Number")
+elif num03==num02 and num02>num01:
+    print("The Second and Third Number are of same value and larger than The First Number")
+else:
+    print("They are all of the same value")
