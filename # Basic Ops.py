e190bf2c32dfbf3e9175a987adc51d0e1aa126d3
Shihab Ahmad <shihabahmadrafi13@gmail.com>
2025-01-28 18:00:02 +0600

    Create # Basic Ops.py

---
+++ # Basic Ops.py
@@ -0,0 +1,12 @@
+# Basic Math Ops
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+print(a + b)  # Addition
+print(a - b)  # Subtraction
+print(a * b)  # Multiplication
+print(a / b)  # Division
+print(a // b) # Floor Division
+print(a % b)  # Modulus
+print(a ** b) # Exponentiation
