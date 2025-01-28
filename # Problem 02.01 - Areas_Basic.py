e1b4bd0e11aef8f0fac2445ad9f018bc8f08f456
Shihab Ahmad <shihabahmadrafi13@gmail.com>
2025-01-28 18:58:15 +0600

    Create # Problem 02.01 - Areas_Basic.py

---
+++ # Problem 02.01 - Areas_Basic.py
@@ -0,0 +1,19 @@
+# Areas_Basic
+
+# Area of a Circle
+radius = float(input("Enter the radius of the circle: "))
+pi = 3.14159
+area = pi * radius * radius
+print("The area of the circle is: ", area)
+
+# Area of a Rectangle
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+area = length * width
+print("The area of the rectangle is: ", area)
+
+# Area of a Triangle
+base = float(input("Enter the base of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+area = 0.5 * base * height
+print("The area of the triangle is: ", area)
