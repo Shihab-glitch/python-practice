99d4a9be0a10ad00b77e03637edf59c5489b2448
Shihab Ahmad <shihabahmadrafi13@gmail.com>
2025-01-28 16:53:44 +0600

    Update # Problem 11 - Sum of all even numbers between 1 and N.py

--- # Problem 11 - Sum of all even numbers between 1 and N.py
+++ # Problem 11 - Sum of all even numbers between 1 and N.py
@@ -7,3 +7,9 @@ while d <= n:
    sum_even += d
    d += 2
 print("Sum of even numbers from 1 to", n, "is", sum_even)
+
+# Basic Code to find sum of all even numbers between 1 and 100
+sum_even = 0
+for i in range(2, 101, 2):
+    sum_even = sum_even + i
+print("Sum of even numbers from 1 to 100 is", sum_even)
