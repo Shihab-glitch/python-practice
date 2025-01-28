dba235d4da951cd447c6fd708101dacfbadc3ba5
Shihab Ahmad <shihabahmadrafi13@gmail.com>
2025-01-28 18:09:48 +0600

    Create # Problem 19 - Password Validator.py

---
+++ # Problem 19 - Password Validator.py
@@ -0,0 +1,9 @@
+# Problem 19 - Password Validator
+
+correct_password = "python123"
+user_password = input("Enter the password: ")
+
+if user_password == correct_password:
+    print("Access Granted.")
+else:
+    print("Access Denied.")
