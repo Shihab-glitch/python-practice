f1d0cdcdf7adf3aaf43a17c23ff5c893a1886204
Shihab Ahmad <shihabahmadrafi13@gmail.com>
2025-01-29 10:17:27 +0600

    Update # Problem 12.01 - Odd index.py

--- # Problem 12.01 - Odd index.py
+++ # Problem 12.01 - Odd index.py
@@ -1,8 +1,8 @@
 # Display characters present at an odd index number
 
 x = str(input("Enter the word - "))
-even_index = []
+odd_index = []
 for i in range(0,len(x)):
     if i % 2 != 0:
-        even_index.append(x[i])
-print(even_index)
+        odd_index.append(x[i])
+print(odd_index)
