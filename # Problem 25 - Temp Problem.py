c42636e00b9ef34463b5c781c7af7a27e456069e
Shihab Ahmad <shihabahmadrafi13@gmail.com>
2025-02-04 19:13:08 +0600

    Update # Problem 25 - Temp Problem.py

--- # Problem 25 - Temp Problem.py
+++ # Problem 25 - Temp Problem.py
@@ -1,7 +1,6 @@
 # Problem 25 - Convert Temperature 
 
 # Celsius to Fahrenheit
-
 c = float(input("Enter the temperature in Celsius : "))
 f = (c * 9/5) + 32
 print("The temperature in Fahrenheit is :", f)
